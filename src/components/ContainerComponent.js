d806bbcc14d7adddfd365a6a55b748a99ac9882f
Vagan Pogosyan <vagan2502@gmail.com>
2022-06-21 19:35:16 -0400

    working on bootstrap layout

--- src/components/ContainerComponent.js
+++ src/components/ContainerComponent.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import CardComponent from './CardComponent';
+import '../css/ContainerComponent.css';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const images = [
   'https://images.unsplash.com/photo-1655826524791-85c778336fa4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80',
   'https://images.unsplash.com/photo-1599420186946-7b6fb4e297f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
   'https://images.unsplash.com/photo-1655803395812-ee6271463c9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80',
+  'https://images.unsplash.com/photo-1638913974023-cef988e81629?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+  'https://images.unsplash.com/photo-1655819132317-78f38b5baf5d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+  'https://images.unsplash.com/photo-1655743851335-7770b475e05a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
 ];
 
 const ContainerComponent = () => {
   return (
-    <>
-      {images.map((url) => {
-        return <CardComponent url={url} key={url} />;
-      })}
-    </>
+    <Container>
+      <div className='row row-cols-lg-4 justify-content'>
+        {images.map((url) => {
+          return (
+            <div className='col '>
+              <CardComponent url={url} key={url} />
+            </div>
+          );
+        })}
+      </div>
+    </Container>
   );
 };
 
