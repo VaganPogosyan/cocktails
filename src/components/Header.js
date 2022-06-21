94154584c1556626d19ba63e27877b8c9cd32e3a
Vagan Pogosyan <vagan2502@gmail.com>
2022-06-21 17:48:31 -0400

    started creating basic Bootstrap components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import CardComponent from './CardComponent';
+import { Navbar, Nav } from 'react-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <Navbar expand='lg'>
+        <Container>
+          <Navbar.Brand href='#'>React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse>
+            <Nav.Link href='#home'>Home</Nav.Link>
+            <Nav.Link href='#link'>Link</Nav.Link>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <CardComponent></CardComponent>
+    </>
+  );
 };
 
 export default Header;
