fda87737aa2c229c2541d9f38a93fa00df570dad
Vagan Pogosyan <vagan2502@gmail.com>
2022-06-21 18:39:18 -0400

    able to map through images urls and display them inside Card components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
+import ContainerComponent from './components/ContainerComponent';
 
 function App() {
   return (
     <>
       <Header />
+      <ContainerComponent />
     </>
   );
 }
--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -2,23 +2,18 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
-const CardComponent = () => {
+const CardComponent = (props) => {
   return (
     <>
       <Card style={{ width: '30%' }}>
-        <Card.Img
-          variant='top'
-          src='https://images.unsplash.com/photo-1655826524791-85c778336fa4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80'
-        />
+        <Card.Img variant='top' src={props.url} />
         <Card.Body>
           <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
+          <Card.Text>Some quick example text to build on the card</Card.Text>
           <Button variant='primary'>Go somewhere</Button>
         </Card.Body>
       </Card>
+      ;
     </>
   );
 };
