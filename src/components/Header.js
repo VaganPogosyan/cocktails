6bc1db79e405c724928dad26155d0c972727422b
Vagan Pogosyan <vagan2502@gmail.com>
2022-06-22 20:57:14 -0400

    styling images

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
+import { ArrowRight } from 'react-bootstrap-icons';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
@@ -9,12 +10,17 @@ const Header = () => {
     <>
       <Navbar bg='dark' variant='dark' expand='sm' sticky='top'>
         <Container>
+          <ArrowRight color='white' />
           <Navbar.Brand href='#'>Cocktails</Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse className=''>
-            <Nav className='me-auto'>
-              <Nav.Link href='#home'>Home</Nav.Link>
-              <Nav.Link href='#link'>Link</Nav.Link>
+          <Navbar.Collapse>
+            <Nav className=''>
+              <Nav.Link className='me' href='#home'>
+                Home
+              </Nav.Link>
+              <Nav.Link className='me' href='#link'>
+                Link
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
