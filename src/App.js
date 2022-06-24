9a3a0d99cac391560cb8304068cd4dd7172f1d46
Vagan Pogosyan <vagan2502@gmail.com>
2022-06-24 00:01:18 -0400

    setting up react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Header from './components/Header';
 import ContainerComponent from './components/ContainerComponent';
+import { LinkComponent } from './components/LinkComponent';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Header />
-      <ContainerComponent />
+      <Router>
+        <Routes>
+          <Route path='/home' element={<ContainerComponent />}></Route>
+          <Route path='/link' element={<LinkComponent />}></Route>
+        </Routes>
+      </Router>
+      {/* <ContainerComponent /> */}
     </>
   );
 }
